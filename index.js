a6de714bddd563c12b4c19a244f6379bee3bf72c
Lisa Knox <lisaknox83@gmail.com>
2024-11-16 20:10:58 -0500

    finished porting to bsky

--- index.js
+++ index.js
@@ -1,60 +1,61 @@
 const puppeteer = require('puppeteer');
+const { BSKY_EMAIL, BSKY_PW } = require('./SECRETS');
 const randomAdjective = require('./getAdjective');
-
-const { COHOST_EMAIL, COHOST_PW } = require('./SECRETS');
+const newDisplayName = `${randomAdjective} dolphin`;
 
 (async () => {
     try {
         //---this line for raspbian---
-        const browser = await puppeteer.launch({ product: 'chrome', executablePath: '/usr/bin/chromium-browser' });
+        // const browser = await puppeteer.launch({ product: 'chrome', executablePath: '/usr/bin/chromium-browser' });
         //---this line for other linux---
-        // const browser = await puppeteer.launch({ headless: false });
-
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
 
-        await page.goto('https://cohost.org/rc/login');
+        await page.goto('https://bsky.app/');
+        await page.waitForNavigation({
+            waitUntil: 'networkidle2',
+        });
 
-        const emailInput = await page.$('input[type="email"]');
-        const passwordInput = await page.$('input[type="password"]');
-        const loginSubmitButton = await page.$('button[type="submit"]');
+        const signInButton = await page.$('button[aria-label="Sign in"]');
+        await signInButton.click();
 
-        await emailInput.type(COHOST_EMAIL);
-        await passwordInput.type(COHOST_PW);
+        const emailInput = await page.$(
+            '#root > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div.css-175oi2r.r-dta0w2 > div > div > div > div > div:nth-child(2) > div.css-175oi2r > div:nth-child(1) > input'
+        );
+        const passwordInput = await page.$(
+            '#root > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div.css-175oi2r.r-dta0w2 > div > div > div > div > div:nth-child(2) > div.css-175oi2r > div:nth-child(2) > input'
+        );
+
+        await emailInput.type(BSKY_EMAIL);
+        await passwordInput.type(BSKY_PW);
+
+        const loginSubmitButton = await page.$(
+            '#root > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div.css-175oi2r.r-dta0w2 > div > div > div > div > div:nth-child(3) > button:nth-child(3)'
+        );
         await loginSubmitButton.click();
 
         await page.waitForNavigation({
             waitUntil: 'networkidle2',
         });
 
-        await page.goto('https://cohost.org/rc/project/edit', {
+        await page.goto('https://bsky.app/profile/rngdolphins.bsky.social', {
             waitUntil: 'networkidle2',
         });
 
-        const newDisplayName = `${randomAdjective} dolphin`;
+        const editProfileButton = await page.$('[aria-label="Edit profile"]');
+        editProfileButton.click();
 
-        const displayNameInput = await page.$('input');
+        await page.waitForTimeout(1000);
+
+        const displayNameInput = await page.$('input[aria-label="Display name"]');
         await displayNameInput.click({ clickCount: 3 });
         await displayNameInput.press('Backspace');
         await displayNameInput.type(newDisplayName);
 
-        const saveChangesButton = await page.$('button[type="submit"]');
+        const saveChangesButton = await page.$('button[aria-label="Save"]');
         await saveChangesButton.click();
 
-        //these two lines are accounting for a bug that has been reported
-        await page.select('select', 'roundrect');
-        await saveChangesButton.click();
-
-        await page.waitForNavigation({
-            waitUntil: 'networkidle2',
-        });
-
-        const newUrl = await page.url();
-
-        if (newUrl !== 'https://cohost.org/rc/project/edit') {
-            console.log(`Cohost display name changed to ${newDisplayName}`);
-        } else {
-            console.log('Display name unchanged, check error log');
-        }
+        console.log(`Cohost display name changed to ${newDisplayName}`);
 
         await browser.close();
     } catch (error) {
